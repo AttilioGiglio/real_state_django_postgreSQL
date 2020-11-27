dc0494da5ff889bfbb03c33bc9a4d203b56ba2e7
attiliogiglio <attilio.g93@gmail.com>
2020-11-27 12:21:22 -0300

    static modulos

--- realstate/settings.py
+++ realstate/settings.py
@@ -119,4 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# It organize and manage all the files from static folders, connecting with the app.
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'btre/static')
+]
