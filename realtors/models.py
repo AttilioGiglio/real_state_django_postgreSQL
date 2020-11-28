f03ecbaf2d01e456da38f1efef7708a834848bb1
attiliogiglio <attilio.g93@gmail.com>
2020-11-28 01:00:18 -0300

    crear superuser y registrar modelos en admin

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
+# blank = do optional the field
 
-# Create your models here.
+class Realtor(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos\%y\%m\%d')
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    state = models.CharField(max_length=50)
+    zipcode = models.BooleanField(default=False)
+    description = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return self.name
