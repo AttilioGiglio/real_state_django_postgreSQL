6e3aa2604a9e8978244290e1b5fd4c05c6a78738
attiliogiglio <attilio.g93@gmail.com>
2020-11-28 18:59:25 -0300

    adding data to admin system

--- realtors/models.py
+++ realtors/models.py
@@ -7,8 +7,10 @@ class Realtor(models.Model):
     photo = models.ImageField(upload_to='photos\%y\%m\%d')
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
-    state = models.CharField(max_length=50)
-    zipcode = models.BooleanField(default=False)
-    description = models.DateTimeField(default=datetime.now, blank=True)
+    email = models.CharField(max_length=50)
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(default=datetime.now, blank=True)
+    
+# Method that gives a name instead of a object, in the admin Realtor section 
     def __str__(self):
         return self.name
